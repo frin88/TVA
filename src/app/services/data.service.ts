bb1b07793b2555bfd962f9fbea3eb698baf9e7f3
frin88 <francesca.bagni@gmail.com>
2021-01-16 12:01:15 +0100

    mockup service and dashbord as main

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor() { 
+  }
+
+  
+  public getData(): any {
+
+    //mockup data
+    return [
+      {"Framework": "Vue", "Stars": "166443", "Released": "2014"},
+      {"Framework": "React", "Stars": "150793", "Released": "2013"},
+      {"Framework": "Angular", "Stars": "62342", "Released": "2016"},
+      {"Framework": "Backbone", "Stars": "27647", "Released": "2010"},
+      {"Framework": "Ember", "Stars": "21471", "Released": "2011"}
+  ];
+  
+  }
+
+}
